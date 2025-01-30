9b557ce1e92046d7061a18b03d1550e777790537
rosgarci1 <150745926+rosgarci1@users.noreply.github.com>
2025-01-30 23:32:47 +0000

    First Commit L5

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,26 @@
+//import scanner
+import java.util.Scanner;
 
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //get user name
+        System.out.print("Enter your name: ");
+        String name;
+        name = scanner.nextLine();
+        //get user age
+        System.out.print("Enter your age: ");
+        int age;
+        age = scanner.nextInt();
+        //get user weight
+        System.out.print("Enter your weight: ");
+        float weight;
+        weight = scanner.nextFloat();
+        //ask if user is a smoker t/f
+        System.out.print("You are a smoker (true/false): ");
+        boolean isSmoker;
+        isSmoker = scanner.nextBoolean();
+        //output user info
+        System.out.print(name + ", " + age + "yrs old, " + weight + "lbs, " + "Smoker: " + isSmoker + ". ");
+    }
+}
